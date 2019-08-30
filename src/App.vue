2547b100c8f2fd5eecce234e9828c37506ab4291
Jacob Dick <jdick@solutionreach.com>
2019-08-30 12:21:26 -0600

    fix sticky components

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,22 @@
   <div id="app">
     <div class="row no-gutters vh-100">
       <div class="col-2 bg-secondary text-light">
-        <nav id="nav" class="navbar navbar-expand navbar-dark bg-dark py-4">
-          <router-link to="/" class="navbar-brand">DataIntel</router-link>
-        </nav>
-        <ul class="nav flex-column">
-          <li class="nav-item">
-            <router-link to="/" class="nav-link text-light">Home</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/about" class="nav-link text-light">About</router-link>
-          </li>
-        </ul>
+        <div class="sticky-top">
+          <nav id="nav" class="navbar navbar-expand navbar-dark bg-dark py-4">
+            <router-link to="/" class="navbar-brand">DataIntel</router-link>
+          </nav>
+          <ul class="nav flex-column">
+            <li class="nav-item">
+              <router-link to="/" class="nav-link text-light">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/about" class="nav-link text-light">About</router-link>
+            </li>
+          </ul>
+        </div>
       </div>
       <div class="col bg-light d-flex flex-column">
-        <nav id="nav" class="navbar navbar-expand navbar-light bg-white d-flex shadow-sm py-4">
+        <nav id="nav" class="navbar navbar-expand navbar-light bg-white d-flex shadow-sm py-4 sticky-top">
           <router-link to="/" class="navbar-brand">Chicago Thread Co</router-link>
           <form class="form-inline mr-auto flex-fill d-flex">
               <input class="form-control mr-sm-3 flex-fill shadow-sm border-0" type="search" placeholder="Search" aria-label="Search">
